7e48489931dfa1f3b1a892d181dec8d3b6ec1ad8
coden256 <den.blackshov@gmail.com>
2019-03-04 19:05:42 +0200

    init

---
+++ desktop/src/com/mygdx/starslicer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.starslicer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.starslicer.StarSlicerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarSlicerGame(), config);
+	}
+}
