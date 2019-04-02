c1ab7cdb752099caf4431d4bad3ad09dd1d5dfc2
coden256 <den.blackshov@gmail.com>
2019-04-02 09:38:22 +0300

    bug fixes

--- waveconfigure/starslicerWaveConfiguration.py
+++ waveconfigure/starslicerWaveConfiguration.py
@@ -1,8 +1,9 @@
 from tkinter import *
+from math import *
 
 
 class GrowthResolver():
-	def __init__(self, init, end, type, initVal=None, endVal=None, typeVal=None):
+	def __init__(self, init=None, end=None, type=None, initVal=-1, endVal=-1, typeVal=-1):
 		self.initVal = initVal
 		self.endVal = endVal
 		self.typeVal = typeVal
@@ -14,13 +15,22 @@ class GrowthResolver():
 		self.initEntry = init
 
 	def init(self):
-		return float(self.initEntry.get()) if not initVal else initVal
+		try :
+			return float(self.initEntry.get())
+		except:
+			return self.initVal
 
 	def end(self):
-		return float(self.endEntry.get()) if not endVal else endVal
+		try:
+			return float(self.endEntry.get())
+		except:
+			return self.endVal
 
 	def type(self):
-		return bool(self.typeEntry.get()) if not typeVal else typeVal
+		try :
+			return self.typeEntry.get()
+		except:
+			return self.typeVal
 
 
 class Application(Frame):
@@ -55,14 +65,14 @@ class Application(Frame):
 			c += 1
 
 	def create_widgets(self):
-		
+
 
 		self.countWith = StringVar()
 		self.countWith.set(None)
 		self.createRadioButtons("Compute By",self.countWith, ['MaxNumber', 'Lifespan'], 1, 0)
 
-		
-		self.maxNumber = GrowthResolver(*self.createProperty("MaxNumber/Lifespan", 2))
+
+		self.temp = GrowthResolver(*self.createProperty("MaxNumber/Lifespan", 2))
 		self.amountPerPeriod = GrowthResolver(*self.createProperty("Amount per spawn", 3))
 		self.period = GrowthResolver(*self.createProperty("Period", 4))
 		self.delay = GrowthResolver(*self.createProperty("Delay",5))
@@ -87,32 +97,73 @@ class Application(Frame):
 		self.result = Text(self, width = 100, height = 30, wrap = WORD)
 		self.result.grid(row = 9, column = 0, columnspan = 4)
 
-	def get_lifespan(self):
-		return maxNumber.init()
+	def countLifespan(self, m, a, p):
+		return p*(m/a - 1)
+
+	def countMaxNumber(self, t, a, p):
+		return a*(t/p + 1)
+
 
 	def compute(self):
-
-		if self.countWith == "Lifespan":
-			self.maxNumber = GrowthResolver()
+		if self.countWith.get() == "Lifespan":
+			print("Lifespaning")
+			self.maxNumber = GrowthResolver(initVal = self.countMaxNumber(self.temp.init(), self.amountPerPeriod.init(), self.period.init()),
+											endVal = self.countMaxNumber(self.temp.end(), self.amountPerPeriod.end(), self.period.end()),
+											typeVal = self.temp.type())
+			self.lifeSpan = self.temp
 		else:
+			print("Maxnumbering")
+			self.maxNumber = self.temp
+			self.lifeSpan = GrowthResolver(initVal = self.countLifespan(self.temp.init(), self.amountPerPeriod.init(), self.period.init()),
+			 								endVal = self.countLifespan(self.temp.end(), self.amountPerPeriod.end(), self.period.end()),
+											typeVal = self.temp.type())
+			print(self.maxNumber.init())
 
 
+
+	def computeExpRate(self, init, end, num):
+		return (end/init)**(1/num)
+
+	def computePolyRate(self, init, end, num):
+		return (end-init)/num
+
+	def computeRate(self, gr, n):
+		if gr.type() == "Exponential":
+			return self.computeExpRate(gr.init(), gr.end(), n)
+		if gr.type() == "Polynomial":
+			return self.computePolyRate(gr.init(), gr.end(), n)
 
 	def computeWave(self):
-		from_wave, to_wave = int(self.from_ent.get()), int(self.to_ent.get())
+		self.compute()
+		try:
+			from_wave, to_wave = int(self.from_ent.get()), int(self.to_ent.get())
+		except:
+			from_wave, to_wave = -1, -1
 
 		message = "Wave #{}".format(from_wave)
-		message += "\nLifespan: {}\nDelay:{}\nPeriod: {}\nAmountPerSpawn:{}\nMaxNumber:{}\nHasContainer:{}\n"\
-		.format(lifespan, self.delay.end(), self.period.end(), self.amountPerPeriod.end(), self.maxNumber.end(), hasContainer)
+		message += "\nLifespan: {} - {}\nDelay:{} - {}\nPeriod: {}- {}\nAmountPerSpawn:{} - {}\nMaxNumber:{} - {}\nHasContainer:{}\n"\
+		.format(self.lifeSpan.end(), self.lifeSpan.type(), self.delay.end(), self.delay.type(),
+		self.period.end(), self.period.type(), self.amountPerPeriod.end(), self.amountPerPeriod.type(),
+		self.maxNumber.end(), self.maxNumber.type(), self.hasContainer.get())
 
+		message += "\n\nWave #{}".format(to_wave)
+		message += "\nLifespan: {}\nDelay:{}\nPeriod: {}\nAmountPerSpawn:{}\nMaxNumber:{}\nHasContainer:{}\n"\
+		.format(self.lifeSpan.init(), self.delay.init(), self.period.init(), self.amountPerPeriod.init(), self.maxNumber.init(), self.hasContainer.get())
+
+		print()
 		message += "\nGrowthRate:"
-		message += "\nWave #{}".format(to_wave)
+		f = from_wave
+		message += "\nLifespan: {}\nDelay:{}\nPeriod: {}\nAmountPerSpawn:{}\nMaxNumber:{}\n"\
+		.format(self.computeRate(self.lifeSpan, f),self.computeRate(self.delay, f),
+				self.computeRate(self.period, f),self.computeRate(self.amountPerPeriod, f),self.computeRate(self.maxNumber, f))
+
+
 
 		self.result.delete(0.0, END)
 		self.result.insert(0.0, message)
 
 	def format(self):
-		container,from_wave,to_wave,lifespan = self.compute()
+		self.compute()
 
 		message = "Wave #{}".format(from_wave)
 		message += "\nLifespan: {}\nDelay:{}\nPeriod: {}\nAmountPerSpawn:{}\nMaxNumber:{}\n".format(t,d,p,n,m)
@@ -126,13 +177,3 @@ root.title("StarSlicer Wave Configuration")
 app = Application(root)
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
-		
